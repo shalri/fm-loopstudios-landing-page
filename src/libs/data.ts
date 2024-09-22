02f8677d006e34612317b3d1c9165871d8dc3a48
shalri <fsaonlinework777@gmail.com>
2024-09-22 15:09:08 +0800

    Create data for page

---
+++ src/libs/data.ts
@@ -0,0 +1,97 @@
+export const navLinks = [
+  {
+    label: "About",
+    href: "#",
+  },
+  {
+    label: "Careers",
+    href: "#",
+  },
+  {
+    label: "Events",
+    href: "#",
+  },
+  {
+    label: "Products",
+    href: "#",
+  },
+  {
+    label: "Support",
+    href: "#",
+  },
+];
+
+export const banner = {
+  copy: "Immersive experiences that deliver",
+};
+
+export const hero = {
+  heading: "The leader in interactive VR",
+  copy: "Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.",
+};
+
+export const creations = [
+  {
+    heading: "Deep earth",
+    desktopImage: "/images/desktop/image-deep-earth.jpg",
+    mobileImage: "/images/mobile/image-deep-earth.jpg",
+  },
+  {
+    heading: "Night arcade",
+    desktopImage: "/images/desktop/image-night-arcade.jpg",
+    mobileImage: "/images/mobile/image-night-arcade.jpg",
+  },
+  {
+    heading: "Soccer team VR",
+    desktopImage: "/images/desktop/image-soccer-team.jpg",
+    mobileImage: "/images/mobile/image-soccer-team.jpg",
+  },
+  {
+    heading: "The grid",
+    desktopImage: "/images/desktop/image-grid.jpg",
+    mobileImage: "/images/mobile/image-grid.jpg",
+  },
+  {
+    heading: "From up above VR",
+    desktopImage: "/images/desktop/image-from-above.jpg",
+    mobileImage: "/images/mobile/image-from-above.jpg",
+  },
+  {
+    heading: "Pocket borealis",
+    desktopImage: "/images/desktop/image-pocket-borealis.jpg",
+    mobileImage: "/images/mobile/image-pocket-borealis.jpg",
+  },
+  {
+    heading: "The curiosity",
+    desktopImage: "/images/desktop/image-curiosity.jpg",
+    mobileImage: "/images/mobile/image-curiosity.jpg",
+  },
+  {
+    heading: "Make it fisheye",
+    desktopImage: "/images/desktop/image-fisheye.jpg",
+    mobileImage: "/images/mobile/image-fisheye.jpg",
+  },
+];
+// TDDO: populate icons frm react-icons
+export const socialLinks = [
+  {
+    label: "facebook",
+    href: "#",
+    icon: "",
+  },
+  {
+    label: "Twitter",
+    href: "#",
+    icon: "",
+  },
+  {
+    label: "Pinterest",
+    href: "#",
+    icon: "",
+  },
+  {
+    label: "Instagram",
+    href: "#",
+    icon: "",
+  },
+];

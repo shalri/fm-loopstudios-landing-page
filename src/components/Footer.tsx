895c2210c930e2268ef4c94f1fb7b5a9d01d848a
shalri <fsaonlinework777@gmail.com>
2024-09-23 21:59:08 +0800

    Mobile Navigation layout and functionality

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,34 +4,31 @@ import SocialLinks from "./SocialLinks";
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white">
-      <div className="w-full">
+    <footer className="grid grid-cols-1 place-items-center bg-black pb-[5.5rem] text-white">
+      {/* <div className="w-full"> */}
+      <div className="mt-[6.375rem]">
         <Link href="/" className="" aria-label="home">
-          <div className="h-10 w-[15rem] bg-[url(/images/logo.svg)] bg-cover" />
+          <div className="h-[1.5rem] w-[9rem] bg-[url(/images/logo.svg)] bg-contain bg-no-repeat" />
         </Link>
+        {/* </div> */}
       </div>
-      <ul className="">
-        <nav className="">
-          <ul className="">
-            {navLinks.map((link) => (
-              <li className="hidden sm:flex" key={link.label}>
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="">
-          <ul className="flex">
-            <SocialLinks
-              links={socialLinks}
-              className="items-center justify-center"
-            />
-          </ul>
-        </div>
-        <div className="">
-          <p className="">&copy; 2021 Loopstudios. All rights reserved.</p>
-        </div>
+      <nav className="mt-9">
+        <ul className="space-y-[18px] text-center">
+          {navLinks.map((link) => (
+            <li className="" key={link.label}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <ul className="mt-[3rem] flex gap-x-[0.625rem]">
+        <SocialLinks links={socialLinks} className="text-3xl" />
       </ul>
+      <div className="mt-3">
+        <p className="text-ls-very-dark-gray">
+          &copy; 2021 Loopstudios. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,90 @@
+"use client";
+import { useMobileNav } from "@/hooks/useMobileNavigation";
+import { useSmallScreen } from "@/hooks/useSmallScreen";
 import { navLinks } from "@/libs/data";
+import { cn } from "@/libs/utils";
+import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
+import React, { useCallback, useRef } from "react";
+
+const NavContent = React.memo(function Navcontent({
+  handleLinkClink,
+}: {
+  handleLinkClink: (e: React.MouseEvent) => void;
+}) {
+  return (
+    <ul className="space-y-[18px] px-6 pb-[34px]">
+      {navLinks.map((link) => (
+        <li
+          className="font-heading text-2xl uppercase text-ls-dark-gray sm:flex"
+          key={link.label}
+        >
+          <Link href={link.href} onClick={handleLinkClink}>
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+});
 
 export default function Header() {
+  const isSmallScreen = useSmallScreen();
+  const navRef = useRef<HTMLDivElement>(null);
+  const { isMobileNavActive, toggleMobileNav, closeMobileNav } = useMobileNav(
+    navRef,
+    isSmallScreen,
+  );
+
+  const mobileNavAnimatonWrapper = useCallback(
+    (children: React.ReactNode) => {
+      return (
+        <AnimatePresence>
+          {isMobileNavActive && (
+            <motion.div
+              key="mobileNav"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="fixed inset-0 content-center bg-black"
+            >
+              {children}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      );
+    },
+    [isMobileNavActive],
+  );
+
+  const handleLinkClink = () => {
+    if (isSmallScreen) {
+      closeMobileNav();
+    }
+  };
   return (
     <header className="z-50 pt-10">
       <div className="flex items-end justify-between px-6">
-        <div className="">
-          <Link href="/" className="" aria-label="home">
+        <div className="z-50">
+          <Link href="/" aria-label="home">
             <div className="h-[1.5rem] w-[9.5rem] bg-[url(/images/logo.svg)] bg-contain bg-no-repeat" />
           </Link>
         </div>
         <nav className="">
-          <ul className="">
-            {navLinks.map((link) => (
-              <li className="hidden sm:flex" key={link.label}>
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            ))}
-          </ul>
+          {isSmallScreen ? (
+            mobileNavAnimatonWrapper(
+              <NavContent handleLinkClink={handleLinkClink} />,
+            )
+          ) : (
+            <NavContent handleLinkClink={handleLinkClink} />
+          )}
         </nav>
         <button
-          className="h-[1.4rem] w-[1.5rem] bg-[url(/images/icon-hamburger.svg)] bg-contain bg-no-repeat"
+          className={cn(
+            "h-[1.4rem] w-[1.5rem] bg-[url(/images/icon-hamburger.svg)] bg-contain bg-no-repeat",
+            isMobileNavActive && "z-50 bg-[url(/images/icon-close.svg)]",
+          )}
+          onClick={toggleMobileNav}
           aria-label="Open menu"
         ></button>
       </div>

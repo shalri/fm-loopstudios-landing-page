5c36395cc1a4a4e63fabeb4705a54ea680ec2c44
shalri <fsaonlinework777@gmail.com>
2024-09-23 14:08:45 +0800

    Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,20 +2,21 @@ import { banner, hero } from "@/libs/data";
 
 export default function Hero() {
   return (
-    <>
-      {/* TODO: uncomment code below... using padding for now for easy alignment */}
-      <section className="-mt-[4.2667rem] h-[41rem] w-full content-center bg-[url(/images/mobile/image-hero.jpg)] bg-contain bg-center bg-no-repeat">
-        <h1 className="mx-6 border-[0.1rem] border-white px-6 pb-[1.12rem] pt-[1.51rem] text-[2.5rem] leading-[0.95] text-white">
+    <article className="pb-10">
+      <section className="-mt-[4.2667rem] h-[41rem] w-full content-center bg-[url(/images/mobile/image-hero.jpg)] bg-cover bg-center bg-no-repeat">
+        <h1 className="small-banner mx-6 border-[0.1rem] border-white px-6 pb-[1.12rem] pt-[1.51rem] text-[2.5rem] leading-[0.95] text-white">
           {banner.copy}
         </h1>
       </section>
-      <section className="">
-        <div className="h-[20rem] w-full bg-[url(/images/mobile/image-interactive.jpg)] bg-contain bg-no-repeat" />
-        <div className="">
-          <h2 className="text-4xl uppercase">{hero.heading}</h2>
-          <p className="">{hero.copy}</p>
+      <section className="mt-[5.875rem]">
+        <div className="mx-6 h-[14rem] w-full bg-[url(/images/mobile/image-interactive.jpg)] bg-contain bg-no-repeat" />
+        <div className="mt-[3.125rem] text-center">
+          <h2 className="text-[2rem] uppercase leading-[0.95]">
+            {hero.heading}
+          </h2>
+          <p className="mt-[1.125rem] px-10 leading-[1.66]">{hero.copy}</p>
         </div>
       </section>
-    </>
+    </article>
   );
 }
